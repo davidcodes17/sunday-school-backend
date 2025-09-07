3d2bcd016150d2176a2657e60922ff50e47e83a2
Areegbe <116895551+davidcodes17@users.noreply.github.com>
2025-09-07 02:40:24 +0100

    fixed bugs

--- src/app.ts
+++ src/app.ts
@@ -174,9 +174,9 @@ app.get("/export-attendance", async (req: Request, res: Response) => {
   }
 });
 
-// const PORT = 4000;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on http://localhost:${PORT}`);
-// });
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
 
 export default app;
